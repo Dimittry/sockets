b6bcb931bc59fc94964d356aad16754e5f8ab47a
olyver <demar.07@gmail.com>
2016-10-20 21:53:15 +0900

    DateClient has been wrote.

--- src/main/java/SocketTuts/DateServerClient/DateClient.java
+++ src/main/java/SocketTuts/DateServerClient/DateClient.java
@@ -1,7 +1,27 @@
 package SocketTuts.DateServerClient;
 
-/**
- * Created by Dimon on 20.10.2016.
- */
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class DateClient {
+
+    private static final int DEFAULT_PORT = 9090;
+
+    public static void main(String[] args) {
+        String serverAddress
+                = JOptionPane.showInputDialog("Enter IP address.");
+        try {
+            Socket s = new Socket(serverAddress, DEFAULT_PORT);
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(s.getInputStream()));
+            String answer = input.readLine();
+            JOptionPane.showMessageDialog(null, answer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
 }

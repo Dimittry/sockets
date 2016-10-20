4c39801c9be9e3bff0956638265ce89850afab64
olyver <demar.07@gmail.com>
2016-10-20 21:42:20 +0900

    DateServerClient, DateServer Commit

---
+++ src/main/java/SocketTuts/App.java
@@ -0,0 +1,13 @@
+package SocketTuts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/SocketTuts/DateServerClient/DateClient.java
@@ -0,0 +1,7 @@
+package SocketTuts.DateServerClient;
+
+/**
+ * Created by Dimon on 20.10.2016.
+ */
+public class DateClient {
+}
